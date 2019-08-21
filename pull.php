380e633a8446f994aff587136de8b64fdc994821
Arzel <arzelcanavate@gmail.com>
2019-08-21 16:18:10 +0200

    FIX errors

--- pull.php
+++ pull.php
@@ -1,5 +1,8 @@
 <?php
-
+ ini_set ('display_errors', 'on');
+ ini_set ('log_errors', 'on');
+ ini_set ('display_startup_errors', 'on');
+ ini_set ('error_reporting', E_ALL);
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
 if ( $_POST['payload'] ) {
