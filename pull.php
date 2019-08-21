753d40d25963fb7edf6d9c2735b80ef35d9e45ec
Arzel <arzelcanavate@gmail.com>
2019-08-21 16:46:07 +0200

    FIX bug

--- pull.php
+++ pull.php
@@ -4,8 +4,11 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 // Use in the “Post-Receive URLs” section of your GitHub repo.
 
-if ( $_POST['payload'] ) {
-	shell_exec("cd /var/www/ && git reset –hard HEAD && git pull");
+if (isset($_POST['payload'])) {
+	//shell_exec("cd /var/www/ && git reset –hard HEAD && git pull");
+}
+else {
+	echo "srry";
 }
 
 ?>
